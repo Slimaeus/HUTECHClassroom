a7471e89ad6358ff7b6a606315d1620b34810b0c
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-04-14 21:53:23 +0700

    refactor: Update ResultsController

--- HUTECHClassroom.API/Controllers/Api/V1/ResultsController.cs
+++ HUTECHClassroom.API/Controllers/Api/V1/ResultsController.cs
@@ -5,10 +5,29 @@ namespace HUTECHClassroom.API.Controllers.Api.V1
     [ApiVersion("1.0")]
     public class ResultsController : BaseApiController
     {
-        [HttpGet]
+        [HttpGet("not-found")]
         public IActionResult GetNotFound()
-        {
-            return NotFound();
-        }
+            => NotFound();
+        [HttpGet("ok")]
+        public IActionResult GetOk()
+            => Ok();
+        [HttpGet("bad-request")]
+        public IActionResult GetBadRequest()
+                    => BadRequest();
+        [HttpGet("no-content")]
+        public IActionResult GetNoContent()
+                    => NoContent();
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnauthorized()
+                    => Unauthorized();
+        [HttpGet("conflict")]
+        public IActionResult GetConflict()
+                    => Conflict();
+        [HttpGet("forbid")]
+        public IActionResult GetForbid()
+                    => Forbid();
+        [HttpGet("InternalServerError")]
+        public IActionResult GetInternalServerError()
+                    => StatusCode(StatusCodes.Status500InternalServerError);
     }
 }
