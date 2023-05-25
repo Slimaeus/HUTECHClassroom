781492efe388d04f49da945b09a1a922c62d0bff
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-05-25 19:43:08 +0700

    refactor: Make Get Mission normal

--- HUTECHClassroom.Application/Missions/Queries/GetMissionsWithPagination/GetMissionsWithPaginationQuery.cs
+++ HUTECHClassroom.Application/Missions/Queries/GetMissionsWithPagination/GetMissionsWithPaginationQuery.cs
@@ -2,7 +2,6 @@
 using HUTECHClassroom.Application.Common.Extensions;
 using HUTECHClassroom.Application.Common.Requests;
 using HUTECHClassroom.Application.Missions.DTOs;
-using HUTECHClassroom.Domain.Interfaces;
 using System.Linq.Expressions;
 
 namespace HUTECHClassroom.Application.Missions.Queries.GetMissionsWithPagination;
@@ -10,14 +9,10 @@ namespace HUTECHClassroom.Application.Missions.Queries.GetMissionsWithPagination
 public record GetMissionsWithPaginationQuery(MissionPaginationParams Params) : GetWithPaginationQuery<MissionDTO, MissionPaginationParams>(Params);
 public class GetMissionsWithPaginationQueryHandler : GetWithPaginationQueryHandler<Mission, GetMissionsWithPaginationQuery, MissionDTO, MissionPaginationParams>
 {
-    private readonly IUserAccessor _userAccessor;
 
-    public GetMissionsWithPaginationQueryHandler(IUnitOfWork unitOfWork, IMapper mapper, IUserAccessor userAccessor) : base(unitOfWork, mapper)
+    public GetMissionsWithPaginationQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
     {
-        _userAccessor = userAccessor;
     }
-    protected override Expression<Func<Mission, bool>> FilterPredicate(GetMissionsWithPaginationQuery query)
-        => x => x.MissionUsers.Any(mu => mu.UserId == _userAccessor.Id);
     protected override Expression<Func<Mission, bool>> SearchStringPredicate(string searchString)
         => x => x.Title.ToLower().Contains(searchString.ToLower()) || x.Description.ToLower().Contains(searchString.ToLower());
     protected override Expression<Func<Mission, object>> OrderByKeySelector()
