f9c99f55f567252ba934e5465b17dd649a9056cc
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-06-05 17:04:34 +0700

    fix: Wrong value of Lecturer Role

--- HUTECHClassroom.Domain/Constants/RoleConstants.cs
+++ HUTECHClassroom.Domain/Constants/RoleConstants.cs
@@ -5,6 +5,6 @@ public static class RoleConstants
     public const string ADMIN = "Administrator";
     public const string DEAN = "Dean";
     public const string TRAINING_OFFICE = "TrainingOffice";
-    public const string LECTURER = "Student";
+    public const string LECTURER = "Lecturer";
     public const string STUDENT = "Student";
 }
