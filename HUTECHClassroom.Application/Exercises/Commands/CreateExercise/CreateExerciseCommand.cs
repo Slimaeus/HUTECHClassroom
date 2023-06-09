849c5a915d4745074ad4b108f003ddd551d53401
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-06-09 10:02:37 +0700

    feat: Auto add all members in classroom to Exercise when creating Exercise

--- HUTECHClassroom.Application/Exercises/Commands/CreateExercise/CreateExerciseCommand.cs
+++ HUTECHClassroom.Application/Exercises/Commands/CreateExercise/CreateExerciseCommand.cs
@@ -1,4 +1,5 @@
 ﻿using HUTECHClassroom.Application.Common.Requests;
+using Microsoft.EntityFrameworkCore;
 
 namespace HUTECHClassroom.Application.Exercises.Commands.CreateExercise;
 
@@ -15,8 +16,24 @@ public record CreateExerciseCommand : CreateCommand
 }
 public class CreateExerciseCommandHandler : CreateCommandHandler<Exercise, CreateExerciseCommand>
 {
+    private readonly IRepository<Classroom> _classroomRepository;
 
     public CreateExerciseCommandHandler(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
     {
+        _classroomRepository = unitOfWork.Repository<Classroom>();
+    }
+
+    protected override async Task ValidateAdditionalField(CreateExerciseCommand request, Exercise entity)
+    {
+        var classroomQuery = _classroomRepository.SingleResultQuery()
+            .Include(i => i.Include(c => c.ClassroomUsers))
+            .AndFilter(x => x.Id == request.ClassroomId);
+
+        var classroom = await _classroomRepository.SingleOrDefaultAsync(classroomQuery);
+
+        foreach (var classroomUser in classroom.ClassroomUsers)
+        {
+            entity.ExerciseUsers.Add(new ExerciseUser { UserId = classroomUser.UserId });
+        }
     }
 }
