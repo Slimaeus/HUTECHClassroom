bd3e7d395f1028967b2819177eb784dbaef8bd7d
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-06-17 17:44:11 +0700

    fix: Get Role for Group in Classroom Endpoints

--- HUTECHClassroom.Application/Classrooms/Queries/GetClassroomGroupsWithPagination/GetClassroomGroupsWithPaginationQuery.cs
+++ HUTECHClassroom.Application/Classrooms/Queries/GetClassroomGroupsWithPagination/GetClassroomGroupsWithPaginationQuery.cs
@@ -1,6 +1,9 @@
 ﻿using EntityFrameworkCore.QueryBuilder.Interfaces;
+using HUTECHClassroom.Application.Common.Interfaces;
+using HUTECHClassroom.Application.Common.Models;
 using HUTECHClassroom.Application.Common.Requests;
 using HUTECHClassroom.Application.Groups.DTOs;
+using HUTECHClassroom.Domain.Interfaces;
 using System.Linq.Expressions;
 
 namespace HUTECHClassroom.Application.Classrooms.Queries.GetClassroomGroupsWithPagination;
@@ -8,12 +11,19 @@ namespace HUTECHClassroom.Application.Classrooms.Queries.GetClassroomGroupsWithP
 public record GetClassroomGroupsWithPaginationQuery(Guid Id, ClassroomPaginationParams Params) : GetWithPaginationQuery<GroupDTO, ClassroomPaginationParams>(Params);
 public class GetClassroomGroupsWithPaginationQueryHandler : GetWithPaginationQueryHandler<Group, GetClassroomGroupsWithPaginationQuery, GroupDTO, ClassroomPaginationParams>
 {
-    public GetClassroomGroupsWithPaginationQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+    private readonly IUserAccessor _userAccessor;
+
+    public GetClassroomGroupsWithPaginationQueryHandler(IUnitOfWork unitOfWork, IMapper mapper, IUserAccessor userAccessor) : base(unitOfWork, mapper)
     {
+        _userAccessor = userAccessor;
     }
     protected override Expression<Func<Group, bool>> FilterPredicate(GetClassroomGroupsWithPaginationQuery query)
     {
         return x => x.ClassroomId == query.Id && (query.Params.UserId == null || query.Params.UserId == Guid.Empty || x.GroupUsers.Any(gu => query.Params.UserId == gu.UserId));
+    }
+    protected override IMappingParams GetMappingParameters()
+    {
+        return new UserMappingParams { UserId = _userAccessor.Id };
     }
     protected override Expression<Func<Group, bool>> SearchStringPredicate(string searchString)
     {
