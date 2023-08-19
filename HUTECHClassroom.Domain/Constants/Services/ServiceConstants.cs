054149911ca935c31586afc24fcaf2c70a6a33cc
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-08-19 16:26:39 +0700

    feat: Add Cloudinary settings key constant

--- HUTECHClassroom.Domain/Constants/Services/ServiceConstants.cs
+++ HUTECHClassroom.Domain/Constants/Services/ServiceConstants.cs
@@ -6,4 +6,5 @@ public static class ServiceConstants
     public const string ENVIRONMENT_KEY = "ASPNETCORE_ENVIRONMENT";
     public const string DEVELOPMENT_ENVIRONMENT_VALUE = "Development";
     public const string CONNECTION_STRING_KEY = "DefaultConnection";
+    public const string CLOUDINARY_SETTINGS_KEY = "Cloudinary";
 }
