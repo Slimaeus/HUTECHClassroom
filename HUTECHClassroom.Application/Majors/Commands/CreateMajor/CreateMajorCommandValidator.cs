4670a7f9bfacdb261a17445f21766d3caa3ce542
Slimaeus <nguyenhongthai28042002@gmail.com>
2025-04-18 16:46:27 +0700

    feat: add checking faculty id for major validators

--- HUTECHClassroom.Application/Majors/Commands/CreateMajor/CreateMajorCommandValidator.cs
+++ HUTECHClassroom.Application/Majors/Commands/CreateMajor/CreateMajorCommandValidator.cs
@@ -1,14 +1,18 @@
-﻿using HUTECHClassroom.Domain.Constants;
+﻿using HUTECHClassroom.Application.Common.Validators.Faculties;
+using HUTECHClassroom.Domain.Constants;
 
 namespace HUTECHClassroom.Application.Majors.Commands.CreateMajor;
 
 public sealed class CreateMajorCommandValidator : AbstractValidator<CreateMajorCommand>
 {
-    public CreateMajorCommandValidator()
+    public CreateMajorCommandValidator(
+        FacultyExistenceByIdValidator facultyIdValidator)
     {
         RuleFor(x => x.Code).NotNull().NotEmpty().MaximumLength(MajorConstants.CODE_MAX_VALUE);
         RuleFor(x => x.Title).NotNull().NotEmpty().MaximumLength(MajorConstants.TITLE_MAX_LENGTH);
         RuleFor(x => x.TotalCredits).GreaterThanOrEqualTo(0);
         RuleFor(x => x.NonComulativeCredits).GreaterThanOrEqualTo(0);
+        RuleFor(x => x.FacultyId)
+            .SetValidator(facultyIdValidator);
     }
 }
--- HUTECHClassroom.Application/Majors/Commands/UpdateMajor/UpdateMajorCommandValidator.cs
+++ HUTECHClassroom.Application/Majors/Commands/UpdateMajor/UpdateMajorCommandValidator.cs
@@ -1,15 +1,19 @@
 ﻿using HUTECHClassroom.Application.Common.Validators;
+using HUTECHClassroom.Application.Common.Validators.Faculties;
 using HUTECHClassroom.Domain.Constants;
 
 namespace HUTECHClassroom.Application.Majors.Commands.UpdateMajor;
 
 public sealed class UpdateMajorCommandValidator : UpdateCommandValidator<UpdateMajorCommand>
 {
-    public UpdateMajorCommandValidator()
+    public UpdateMajorCommandValidator(
+        FacultyExistenceByIdValidator facultyIdValidator)
     {
         RuleFor(x => x.Code).MaximumLength(MajorConstants.CODE_MAX_VALUE);
         RuleFor(x => x.Title).MaximumLength(MajorConstants.TITLE_MAX_LENGTH);
         RuleFor(x => x.TotalCredits).GreaterThanOrEqualTo(0);
         RuleFor(x => x.NonComulativeCredits).GreaterThanOrEqualTo(0);
+        RuleFor(x => x.FacultyId)
+            .SetValidator(facultyIdValidator);
     }
 }
